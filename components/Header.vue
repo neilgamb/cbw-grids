805a679b9c576084a786bd76b1503d821e0ca3dd
Neilson <ngamble@redventures.com>
2019-03-21 18:46:45 -0400

    adding carousel to header

--- components/Header.vue
+++ components/Header.vue
@@ -1,22 +1,22 @@
 <template>
-  <v-toolbar
-    clipped-left
-    fixed
-    app
-    flat
-  >
-    <v-toolbar-side-icon @click="handleDrawerToggle" />
-    <v-toolbar-title>CBW Grids</v-toolbar-title>
-    <v-spacer />
-    <v-toolbar-items>
+  <v-toolbar dark fixed app>
+    <v-toolbar-title class="titleContainer">
+      <v-toolbar-side-icon @click="handleDrawerToggle" />
+    </v-toolbar-title>
+    <div v-if="!isMobile" class="manualDateNav">
+      <v-btn icon color="primary darken-2" @click="currentDayDown">
+        <v-icon>keyboard_arrow_left</v-icon>
+      </v-btn>
+      <v-btn icon color="primary darken-2" @click="currentDayUp">
+        <v-icon>keyboard_arrow_right</v-icon>
+      </v-btn>
+    </div>
+    <div class="carouselContainer">
       <carousel
-        :navigateTo="currentDay"
+        :navigate-to="currentDay"
         :per-page="1"
-        :mouse-drag="true"
-        :paginationEnabled="false"
-        :scrollPerPage="true"
-        paginationActiveColor="#f0f0f0"
-        paginationColor="#666"
+        :mouse-drag="false"
+        :pagination-enabled="false"
         @pageChange="setCurrentDay"
       >
         <slide v-for="date in dates" :key="date.date.toString()" class="date-container">
@@ -33,7 +33,7 @@
           </div>
         </slide>
       </carousel>
-    </v-toolbar-items>
+    </div>
   </v-toolbar>
 </template>
 
@@ -53,7 +53,10 @@ export default {
       dates: 'general/dates',
       currentDay: 'general/currentDay',
       currentPeriod: 'general/currentPeriod'
-    })
+    }),
+    isMobile() {
+      return window.innerWidth < 1024
+    }
   },
   methods: {
     ...mapActions({
@@ -92,3 +95,85 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+// @import "../scss/_mixins.scss";
+// @import "../scss/variables.scss";
+
+/deep/ .v-toolbar__content {
+  padding: 0px;
+}
+
+.titleContainer {
+  // height: 150%;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+
+  .logoContainer {
+    position: relative;
+    width: 50%;
+
+    .logo-jazzfest {
+      // font-family: "Rock Salt", cursive;
+      // color: $primary;
+      font-size: 20px;
+      line-height: 23px;
+      // text-shadow: 1px 1px $black;
+      transform: rotate(-32deg);
+      z-index: 2;
+      position: absolute;
+      top: 20px;
+      left: 10px;
+    }
+
+    .logo-grids {
+      font-family: "Monoton", cursive;
+      font-size: 35px;
+      // color: lighten($secondary, 10%);
+      // animation: glow 3s ease-in-out infinite alternate;
+      position: absolute;
+      top: 20px;
+      left: 60px;
+    }
+  }
+}
+
+.manualDateNav {
+  display: flex;
+}
+
+.carouselContainer {
+  width: 40%;
+  max-width: 200px;
+
+  .date-container {
+    display: flex;
+    justify-content: space-around;
+
+    .day-of-week {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      // font-family: "Sedgwick Ave", cursive;
+      font-size: 22px;
+    }
+
+    .calendar-container {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+
+      .month {
+        font-size: 10px;
+      }
+
+      .day {
+        font-size: 20px;
+        line-height: 20px;
+      }
+    }
+  }
+}
+</style>
